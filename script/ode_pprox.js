2c79f6cf18ea5edb37dcd69c43b7897f4dfc133f
g7x9a <g7x9a@ugrad.cs.ubc.ca>
2019-11-26 14:45:31 -0800

    Cleaned up and documented the script file

--- script/ode_pprox.js
+++ script/ode_pprox.js
@@ -1,46 +1,48 @@
-//window.addEventListener('load',function(){
+// Wait untill DOM is properly loaded
 window.addEventListener('DOMContentLoaded', function () {
-    //window.onload = function () {
 
-    document.getElementById("euler-button").addEventListener("click", function () {
+    approx_methodes = document.getElementsByClassName("button");
 
-        let f_prime = document.getElementById("formula").querySelector("input").value;
-        let h = document.getElementById("stepsize").querySelector("input").value;
-        let x_0 = document.getElementById("initial-conditions").querySelector("input#x").value;
-        let y_0 = document.getElementById("initial-conditions").querySelector("input#y").value;
-        let x_final = document.getElementById("final-conditions").querySelector("input").value;
-        params_valid = checkParameters(f_prime, h, x_0, y_0, x_final);
+    for (var i = 0; i < approx_methodes.length; i++) {
 
-        if (params_valid.isValid) {
-            eulerMethode(f_prime, parseFloat(h), parseFloat(x_0), parseFloat(y_0), parseFloat(x_final));
-        } else {
-            alert(params_valid.errorMessage);
-        }
-    });
-    document.getElementById("rungekutta-button").addEventListener("click", function () {
-        
-        let f_prime = document.getElementById("formula").querySelector("input").value;
-        let h = document.getElementById("stepsize").querySelector("input").value;
-        let x_0 = document.getElementById("initial-conditions").querySelector("input#x").value;
-        let y_0 = document.getElementById("initial-conditions").querySelector("input#y").value;
-        let x_final = document.getElementById("final-conditions").querySelector("input").value;
+        approx_methodes[i].addEventListener("click", function () {
+            let f_prime = document.getElementById("formula").querySelector("input").value;
+            let h = document.getElementById("stepsize").querySelector("input").value;
+            let x_0 = document.getElementById("initial-conditions").querySelector("input#x").value;
+            let y_0 = document.getElementById("initial-conditions").querySelector("input#y").value;
+            let x_final = document.getElementById("final-conditions").querySelector("input").value;
+            params_valid = checkParameters(f_prime, h, x_0, y_0, x_final);
 
-        params_valid = checkParameters(f_prime, h, x_0, y_0, x_final);
+            if (params_valid.isValid) {
+                if (this.id === "euler-button") {
+                    eulerMethode(f_prime, parseFloat(h), parseFloat(x_0), parseFloat(y_0), parseFloat(x_final));
+                } else {
+                    runegekuttaMethode(f_prime, parseFloat(h), parseFloat(x_0), parseFloat(y_0), parseFloat(x_final));
+                }
 
-        if (params_valid.isValid) {
-            runegekuttaMethode(f_prime, parseFloat(h), parseFloat(x_0), parseFloat(y_0), parseFloat(x_final));
-        } else {
-            alert(params_valid.errorMessage);
-        }
-    });
+            } else {
+                alert(params_valid.errorMessage);
+            }
+        })
 
+    }
 });
-//}
 
+/**
+ * This function determines if given inputs are valid.
+ * @param {String}      f_prime     Determine if given formula is of the correct format
+ *                                  and can be evaluated by eval()   
+ * @param {Float}       h           Determine if given input is a valid float
+ * @param {Float}       x_0         Determine if given input is a valid float
+ * @param {Float}       y_0         Determine if given input is a valid float
+ * @param {Float}       x_final     Determine if given input is a valid float
+ * 
+ * @returns {Object}
+ */
 function checkParameters(f_prime, h, x_0, y_0, x_final) {
     var params_validity = {
         errorMessage: "",
-        isValid:true,
+        isValid: true,
     };
 
     try {
@@ -72,9 +74,19 @@ function checkParameters(f_prime, h, x_0, y_0, x_final) {
         params_validity.isValid = false;
     }
 
-    return params_validity ;
+    return params_validity;
 }
 
+/**
+ * This function uses the Euler methode to approximate an Ordinary Differential Equation,
+ * then display it on the screen for the user. 
+ * Read more about it here: https://en.wikipedia.org/wiki/Euler_method
+ * @param {String}      f_prime     Ordinary differential Equation of the form y'=f(x,y)  
+ * @param {Float}       h           Euler step size. 
+ * @param {Float}       x_0         Initial condition x
+ * @param {Float}       y_0         Initial condition y
+ * @param {Float}       x_final     Final condition y
+ */
 function eulerMethode(f_prime, h, x_0, y_0, x_final) {
 
     for (var x = x_0 + h, y = y_0; x <= x_final; x += h) {
@@ -85,6 +97,16 @@ function eulerMethode(f_prime, h, x_0, y_0, x_final) {
     alert(y);
 }
 
+/**
+ * This function uses the Runge_Kutta methode to approximate an Ordinary Differential Equation,
+ * then display it on the screen for the user. 
+ * Read more about it here: https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
+ * @param {String}      f_prime     Ordinary differential Equation of the form y'=f(x,y)  
+ * @param {Float}       h           Euler step size. 
+ * @param {Float}       x_0         Initial condition x
+ * @param {Float}       y_0         Initial condition y
+ * @param {Float}       x_final     Final condition y
+ */
 function runegekuttaMethode(f_prime, h, x_0, y_0, x_final) {
 
     for (var x_temp = x_0, y_temp = y_0; x_temp <= x_final; x_temp += h) {
